d88f80905f2761990c32fea32c2df72a91038085
robertojff <robertojff3333@gmail.com>
2023-12-16 20:21:24 -0300

    first commit

---
+++ arrays.js
@@ -0,0 +1,17 @@
+let familia = [22,32,35,40];
+console.log(familia[2]);
+
+let segundaFamilia = [10,12,43,45];
+console.log(segundaFamilia[1]);
+
+let terceiraFamilia = [33,8,36];
+console.log(terceiraFamilia[0]);
+
+let primeiraPessoa = ['Roberto',22,1.70,];
+console.log(primeiraPessoa);
+
+let segundaPessoa = ['Neusa',62,1.65]
+console.log(segundaPessoa);
+
+let terceriaPessoa = ['Adalberto',67,1.67]
+console.log(terceriaPessoa[1]);
